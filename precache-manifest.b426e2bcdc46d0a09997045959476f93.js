11adafb6258ecb5adde50865ac6d9b76c1cef460
Yashaswita <yashaswitasingh333@gmail.com>
2020-07-18 23:10:43 +0530

    Updates

---
+++ precache-manifest.b426e2bcdc46d0a09997045959476f93.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3900a113d6d7caef31584dfad101e600",
+    "url": "/restaurantWebsite/index.html"
+  },
+  {
+    "revision": "8ff5c7274ac650a92d6a",
+    "url": "/restaurantWebsite/static/css/2.cc7a06a6.chunk.css"
+  },
+  {
+    "revision": "01927b78b243c27cdcae",
+    "url": "/restaurantWebsite/static/css/main.62c9f8b0.chunk.css"
+  },
+  {
+    "revision": "8ff5c7274ac650a92d6a",
+    "url": "/restaurantWebsite/static/js/2.c81462cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/restaurantWebsite/static/js/2.c81462cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01927b78b243c27cdcae",
+    "url": "/restaurantWebsite/static/js/main.45ebdbda.chunk.js"
+  },
+  {
+    "revision": "ab969f3e8420e257a4e7",
+    "url": "/restaurantWebsite/static/js/runtime-main.25102327.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurantWebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurantWebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurantWebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurantWebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurantWebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
